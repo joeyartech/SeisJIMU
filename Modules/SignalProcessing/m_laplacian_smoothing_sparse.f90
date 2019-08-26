137c7a99942ba85b1878951d083f498c3957d415
joeyartech <joeywzhou1986@gmail.com>
2019-08-26 14:06:55 -0500

    modif:m_laplacian_smoothing_sparse.f90:change parameters to be benchmarked with toyxdac_time's smoothing, archive old procedures in devel/

--- Modules/SignalProcessing/m_laplacian_smoothing_sparse.f90
+++ Modules/SignalProcessing/m_laplacian_smoothing_sparse.f90
@@ -1,7 +1,7 @@
 module m_laplacian_smoothing_sparse
 use m_sysio
 
-    private pi,nz,nx,ny,iaddmirror,dz,dx,dy,freq,frac_z,frac_x,frac_y,is_cubic,scaling
+    private pi,nz,nx,ny,iaddmirror,dz,dx,dy,freq,frac_z,frac_x,frac_y,is_cubic,preserve
     public
     
     real,parameter :: pi=3.1415927
@@ -11,50 +11,11 @@ use m_sysio
     real    :: freq
     real    :: frac_z,frac_x,frac_y
     logical :: is_cubic
-    
-    real,parameter :: scaling=0.2 !scaling factor to convert fraction of half wavelength to standard deviation. This magic number assures the gradient is multiplified by ~0.001 by the laplacian or gaussian functions half frac_wavelength away from the center (note that these two functions do not have max=1 due to normalization)
+    character(6) :: preserve
 
     contains
     
-    !analytical version, preserves vector's energy after smoothing but can amplify values at the end (e.g. could be dangerous at the seafloor)
-!     subroutine smooth_1D_stationary(n, h, vector, b)
-!         integer n  !vector length
-!         real    h  !sampling rate
-!         real,dimension(n) :: vector !array to be smoothed
-!         
-!         real    b !scale, variance = 2b^2
-!         real,dimension(n) :: d !diagonal terms
-!         real,dimension(n) :: du,dl  !super- and sub-diagonal terms
-!         
-!         du=-b/h**3
-!         d=1./b/h+2*b/h**3
-!         dl=-b/h**3
-! 
-!         !Neumann boundary conditions on two ends to avoid leakage after the smoothing
-!         d(1)=d(1)-b/h**3
-!         d(n)=d(n)-b/h**3
-!         
-!         call sgtsv( n, 1, dl(2:n), d(:), du(1:n-1), vector, n, info )
-!         
-!         renormalization
-!         vector=vector*2/h**2
-!         
-!         if(info /= 0 ) then
-!             !call hud('Laplacian smoothing has a problem')
-!             if(info<0) then
-!                 write(*,*) '-i=',info, ': The i-th argument had an illegal value'
-!             else
-!                 write(*,*) 'i=',info, ': U(i,i) is exactly zero, and the solution has not been computed. The factorization has not been completed unless i = N.'
-!             endif
-!             stop 'stopped now'
-!         endif
-!         
-!         !renormalization
-!         vector=vector*0.5*h/b
-!         
-!     end subroutine
     
-    !`damped version', no preservation of energy but quite precise to the discretized Laplacian function and no value amplications at the end (e.g. safe at the seafloor)
     subroutine smooth_1D_stationary(n, h, vector, b)
         integer n  !vector length
         real    h  !sampling rate
@@ -66,13 +27,13 @@ use m_sysio
         
         tmp=1./(exp(h/b)-exp(-h/b))
         
-        du=-1.*tmp
+        du=   -tmp
         d = 1.+2.*exp(-h/b)*tmp
-        dl=-1.*tmp
+        dl=   -tmp
         
         !Neumann boundary conditions on two ends to avoid leakage after the smoothing
-        d(1)=d(1)-b/h**3
-        d(n)=d(n)-b/h**3
+        d(1)=d(1)+dl(1)
+        d(n)=d(n)+du(n)
         
         call sgtsv( n, 1, dl(2:n), d(:), du(1:n-1), vector, n, info )
         
@@ -87,7 +48,7 @@ use m_sysio
         endif
         
         !normalization
-        vector=vector*0.5*h/b
+        vector=vector/b*0.39894228
         
     end subroutine
     
@@ -101,23 +62,22 @@ use m_sysio
         real,dimension(n) :: du,dl  !super- and sub-diagonal terms
         
         do i=1,n
-            b=scaling*frac_lambda(i)/sqrt(2.)  !b=std/sqrt(2)
+            b=frac_lambda(i)/2.
             
             tmp=1./(exp(h/b)-exp(-h/b))
             
-            du(i)=-1.*tmp
+            du(i)=   -tmp
             d(i) = 1.+2.*exp(-h/b)*tmp
-            dl(i)=-1.*tmp
+            dl(i)=   -tmp
         enddo
         
         !Neumann boundary conditions on two ends to avoid edge effect after the smoothing
-        d(1)=d(1)-dl(1)
-        d(n)=d(n)-du(n)
+        d(1)=d(1)+dl(1)
+        d(n)=d(n)+du(n)
         
         call sgtsv( n, 1, dl(2:n), d(:), du(1:n-1), vector, n, info )
         
         if(info /= 0 ) then
-            !call hud('Gaussian smoothing problem along z direction')
             if(info<0) then
                 write(*,*) '-i=',info, ': The i-th argument had an illegal value'
             else
@@ -128,19 +88,20 @@ use m_sysio
         
         !normalization
         do i=1,n
-           b=scaling*frac_lambda(i)/sqrt(2.)  !b=std/sqrt(2)
-           vector(i)=vector(i)*0.5*h/b
+           b=frac_lambda(i)/2.
+           vector(i)=vector(i)/b*0.39894228
         enddo
         
     end subroutine
     
     
-    subroutine init_laplacian_smoothing(n,d,frequency,o_addmirror,o_frac)
+    subroutine init_laplacian_smoothing(n,d,frequency,o_addmirror,o_frac,o_preserve)
         integer,dimension(3) :: n
         real,dimension(3) :: d
         
         real,optional :: o_addmirror
         real,dimension(3),optional :: o_frac
+        character(6),optional :: o_preserve
         
         character(:),allocatable :: tmp
         
@@ -169,6 +130,12 @@ use m_sysio
         else
             tmp=get_setup_char('SMOOTH_GRADIENT_WAVELENGTH_FRACTION',default='1 1 1')
             read(tmp,*)frac_z,frac_x,frac_y
+        endif
+        
+        if(present(o_preserve)) then
+            preserve=o_preserve
+        else
+            preserve=get_setup_char('SMOOTH_GRADIENT_PRESERVE_MAGNITUDE',default='L2norm')
         endif
         
     end subroutine
@@ -236,21 +203,19 @@ use m_sysio
             deallocate(vector)
         endif
         
-        !renormalization
-        if(is_cubic) then
-            gradient=gradient/pi !due to approximation this theoretical scaling factor is no more valid..
-            !gradient=gradient*norm/sum(abs(gradient))
-        else
-            gradient=gradient*2./pi !due to approximation this theoretical scaling factor is no more valid..
-            !gradient=gradient*norm/sum(abs(gradient))
-        endif
-        
     end subroutine
     
     subroutine laplacian_smoothing_pseudo_nonstationary(gradient,velocity)
         real,dimension(nz,nx,ny) :: gradient,velocity
         
         real,dimension(:),allocatable :: vector,frac_lambda
+        real :: old_norm
+        
+        if(preserve=='L1norm') then
+            old_norm = sum(abs(gradient))
+        elseif(preserve=='L2norm') then
+            old_norm = norm2(gradient)
+        endif
         
         !z axis
         allocate(vector(nz),frac_lambda(nz))
@@ -290,11 +255,12 @@ use m_sysio
             deallocate(vector,frac_lambda)
         endif
         
-        !renormalization
-        if(is_cubic) then
-            gradient=gradient/pi
-        else
-            gradient=gradient*pi
+        !normalization
+        if(preserve=='L1norm') then
+            gradient = gradient * old_norm / sum(abs(gradient))
+        elseif(preserve=='L2norm') then
+            gradient = gradient * old_norm / norm2(gradient)
+        !elseif(preserve=='scale1') then !don't expect this behavior is better..
         endif
         
     end subroutine
