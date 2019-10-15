40de717b8aa543dae867855a4328d9ad49f6f7da
joeyartech <joeyartech@gmail.com>
2019-10-15 18:40:08 -0500

    modif:m_boundarystore.f90:save 3 inner points instead of 1 CPML point + 2 inner points

--- Modules/Modeling/TimeDomain/FDSG/m_boundarystore.f90
+++ Modules/Modeling/TimeDomain/FDSG/m_boundarystore.f90
@@ -10,6 +10,7 @@ use m_field, only: t_field
 
     type t_boundarystore
         real,dimension(:,:),allocatable :: vz_top,vz_bot,vx_left,vx_right,vy_front,vy_rear
+        real,dimension(:,:),allocatable :: vx_top,vx_bot,vz_left,vz_right
     end type
     
     type(t_boundarystore) :: bnd
@@ -31,10 +32,14 @@ use m_field, only: t_field
         
         n=3*nx*ny
         call alloc(bnd%vz_top,n,nt)
+        call alloc(bnd%vx_top,n,nt)
         call alloc(bnd%vz_bot,n,nt)
+        call alloc(bnd%vx_bot,n,nt)
         n=nz*3*ny
         call alloc(bnd%vx_left, n,nt)
+        call alloc(bnd%vz_left, n,nt)
         call alloc(bnd%vx_right,n,nt)
+        call alloc(bnd%vz_right, n,nt)
         if(m%is_cubic) then
             n=nz*nx*3
             call alloc(bnd%vy_front,n,nt)
@@ -53,50 +58,54 @@ use m_field, only: t_field
         ny=cb%my
         if(m%is_cubic) then
             !top
-            call bndcpy(action,f%vz,bnd%vz_top(:,it),[0,2],    [1,nx],[1,ny])
+            call bndcpy(action,f%vz,bnd%vz_top(:,it),[1,3],    [1,nx],[1,ny])  !old version: [0,2],[1,nx],[1,nx]
             !bottom
-            call bndcpy(action,f%vz,bnd%vz_bot(:,it),[nz,nz+2],[1,nx],[1,ny])
+            call bndcpy(action,f%vz,bnd%vz_bot(:,it),[nz-2,nz],[1,nx],[1,ny])  !old version: [nz,nz+2],[1,nx],[1,nx]
             !left
-            call bndcpy(action,f%vx,bnd%vx_left(:,it), [1,nz],[0,2],    [1,ny])
+            call bndcpy(action,f%vx,bnd%vx_left(:,it), [1,nz],[1,3],    [1,ny])
             !right
-            call bndcpy(action,f%vx,bnd%vx_right(:,it),[1,nz],[nx,nx+2],[1,ny])
+            call bndcpy(action,f%vx,bnd%vx_right(:,it),[1,nz],[nx-2,nx],[1,ny])
             !front
-            call bndcpy(action,f%vy,bnd%vy_front(:,it),[1,nz],[1,nx],[0,2])
+            call bndcpy(action,f%vy,bnd%vy_front(:,it),[1,nz],[1,nx],[1,3])
             !rear
-            call bndcpy(action,f%vy,bnd%vy_rear(:,it), [1,nz],[1,nx],[ny,ny+2])
+            call bndcpy(action,f%vy,bnd%vy_rear(:,it), [1,nz],[1,nx],[ny-2,ny])
         else
             !top
-            call bndcpy(action,f%vz,bnd%vz_top(:,it),[0,2],    [1,nx],[1,1])
+            call bndcpy(action,f%vz,bnd%vz_top(:,it),[1,3],    [1,nx],[1,1])
+            call bndcpy(action,f%vx,bnd%vx_top(:,it),[1,3],    [1,nx],[1,1])
             !bottom
-            call bndcpy(action,f%vz,bnd%vz_bot(:,it),[nz,nz+2],[1,nx],[1,1])
+            call bndcpy(action,f%vz,bnd%vz_bot(:,it),[nz-2,nz],[1,nx],[1,1])
+            call bndcpy(action,f%vx,bnd%vx_bot(:,it),[nz-2,nz],[1,nx],[1,1])
             !left
-            call bndcpy(action,f%vx,bnd%vx_left(:,it), [1,nz],[0,2],    [1,1])
+            call bndcpy(action,f%vx,bnd%vx_left(:,it), [1,nz],[1,3],    [1,1])
+            call bndcpy(action,f%vz,bnd%vz_left(:,it), [1,nz],[1,3],    [1,1])
             !right
-            call bndcpy(action,f%vx,bnd%vx_right(:,it),[1,nz],[nx,nx+2],[1,1])
+            call bndcpy(action,f%vx,bnd%vx_right(:,it),[1,nz],[nx-2,nx],[1,1])
+            call bndcpy(action,f%vz,bnd%vz_right(:,it),[1,nz],[nx-2,nx],[1,1])
         endif
         
     end subroutine
     
-    subroutine bndcpy(action,v,bv,nz,nx,ny)
+    subroutine bndcpy(action,v,bv,iiz,iix,iiy)
         character(4) :: action
         real,dimension(cb%n) :: v
         real,dimension(*) :: bv
-        integer,dimension(2),intent(in) :: nz,nx,ny
+        integer,dimension(2),intent(in) :: iiz,iix,iiy
         
-        ifz=nz(1); ilz=nz(2)
-        ifx=nx(1); ilx=nx(2)
-        ify=ny(1); ily=ny(2)
+        ifz=iiz(1); ilz=iiz(2)
+        ifx=iix(1); ilx=iix(2)
+        ify=iiy(1); ily=iiy(2)
         
-        nnz=nz(2)-nz(1)+1
-        nnx=nx(2)-nx(1)+1
-        nny=ny(2)-ny(1)+1
+        nz=iiz(2)-iiz(1)+1
+        nx=iix(2)-iix(1)+1
+        ny=iiy(2)-iiy(1)+1
         
         if(action=='save') then !save
             do iy=ify,ily
             do ix=ifx,ilx
             do iz=ifz,ilz
                 i = (iz-cb%ifz) + (ix-cb%ifx)*cb%nz + (iy-cb%ify)*cb%nz*cb%nx +1 !field indexing
-                k = (iz-ifz)    + (ix-ifx)*nnz      + (iy-ify)*nnz*nnx +1 !boundary_field indexing
+                k = (iz-ifz)    + (ix-ifx)*nz       + (iy-ify)*nz*nx +1 !boundary_field indexing
                 
                 bv(k) = v(i)
             enddo
@@ -108,7 +117,7 @@ use m_field, only: t_field
             do ix=ifx,ilx
             do iz=ifz,ilz
                 i = (iz-cb%ifz) + (ix-cb%ifx)*cb%nz + (iy-cb%ify)*cb%nz*cb%nx +1
-                k = (iz-ifz)    + (ix-ifx)*nnz      + (iy-ify)*nnz*nnx +1
+                k = (iz-ifz)    + (ix-ifx)*nz      + (iy-ify)*nz*nx +1
                 
                 v(i) = bv(k)
             enddo
