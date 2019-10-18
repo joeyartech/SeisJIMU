e5b97924323e4b4616ce6da840c21d16487882b5
joeyartech <joeyartech@gmail.com>
2019-10-17 19:10:40 -0500

    modif:m_parameterization.f90:reconstruct this module to allow vs inversion, and alow e.g. rho only inversion. now the sequence of ACTIVE_PARAMETER value in the setup.in DOES matter as the scaling variable in the linesearch_scaling subroutine of m_linearsearcher*.f90 will depend on the first par listed in ACTIVE_PARAMETER, which may not be vp

--- Modules/Optimization/m_parameterization.f90
+++ Modules/Optimization/m_parameterization.f90
@@ -2,88 +2,164 @@ module m_parameterization
 use m_sysio
 use m_arrayop
 use m_model
+use m_field, only:waveeq_info
 use m_gradient, only: gradient
 
+    !PARAMETERIZATION                              !allowed ACTIVE_PARAMETER
+    !WaveEquation:  moduli-density:                 kpa-rho or lda-mu-rho
+    !I/O models:    velocities-density:             vp-rho or vp-vs-rho
+    !Empirical:     velocities-density-empirical
+    !seismic:       velocities-impedances           vp-ip or vp-vs-ip
+    !tomography:    slowness-density                sp-rho
+    !optimization:  can be any of above
+
+    !optim x =FWD=> model m =FWD=> WaveEq lm =PARAMETERIZATION=> optim x,g
+
+    !Symbol meaning:
+    !vp    P-wave velocity
+    !vs    S-wave velocity
+    !sp    P-wave slowness
+    !ip    P-wave (acoustic) impedance
+    !rho   density
+    !kpa   bulk modulus
+    !lda   1st Lamé parameter
+    !mu    2nd Lamé parameter, shear modulus
+
     public
+    private :: active, read_active, check_par
     
     character(:),allocatable :: parameterization, active
     
-    logical :: if_par_vp=.false., if_par_rho=.false., if_par_ip=.false., if_par_kpa=.false.
-    
-    real :: par_vp_min,  par_vp_max
-    real :: par_rho_min, par_rho_max
-    real :: par_ip_min,  par_ip_max
-    real :: par_kpa_min, par_kpa_max
+    character(3),dimension(3) :: pars !max 3 letters for each par, max 3 parameters
+    real,dimension(3) :: pars_min
+    real,dimension(3) :: pars_max
     
     integer :: npar
 
     contains
     
     subroutine init_parameterization
+                
+        parameterization=get_setup_char('PARAMETERIZATION',default='velocities-density')
+
+        active=get_setup_char('ACTIVE_PARAMETER',default='vp1500:3400')
+
+        active=trim(adjustl(active)) !no head or tail spaces
+
+        !read in parameters and their allowed ranges
+        call read_active
+
+        ! !adjust parameters according to parameterization
+        ! call adjust_pars
         
-        parameterization=get_setup_char('PARAMETERIZATION',default='vp-rho')
-        
-        active=get_setup_char('ACTIVE_PARAMETER',default='vp1500-3400')
-        
-        !turn on inv pars and read in value ranges
-        call read_active_par('vp',   if_par_vp, par_vp_min, par_vp_max)
-        call read_active_par('rho',  if_par_rho,par_rho_min,par_rho_max)
-        call read_active_par('ip',   if_par_ip, par_ip_min, par_ip_max)
-        call read_active_par('kappa',if_par_kpa,par_kpa_min,par_kpa_max)
-        
-        !turn off inv pars depending on parameterization
-        select case (parameterization)
-            case ('kappa-rho')
-                if_par_vp=.false.
-                if_par_ip=.false.
-            case ('vp-rho')
-                if_par_kpa=.false.
-                if_par_ip=.false.
-            case ('vprho-gardner')
-                if_par_kpa=.false.
-                if_par_rho=.false.
-                if_par_ip=.false.
-            case ('vp-ip')
-                if_par_kpa=.false.
-                if_par_rho=.false.
-        end select
-        
-        !reallocate model if initial model is not provided
-        if(if_par_rho.or.if_par_kpa.or.if_par_ip) then
-            call model_reallocate('rho')
-        endif
-        
-        npar=count([if_par_kpa,if_par_rho,if_par_vp,if_par_ip].eqv..true.)
-        if(mpiworld%is_master) write(*,*) 'Number of inversion parameters:', npar
-        
+        ! !reallocate model if initial model is not provided
+        ! if(if_par_rho.or.if_par_kpa.or.if_par_ip) then
+        !     call model_reallocate('rho')
+        ! endif
+        ! 
+        ! npar=count([if_par_kpa,if_par_rho,if_par_vp,if_par_ip].eqv..true.)
+        ! if(mpiworld%is_master) write(*,*) 'Number of inversion parameters:', npar
+
     end subroutine
     
-    subroutine read_active_par(par,if_par,amin,amax)
-        character(*),intent(in) :: par
-        logical,intent(out) :: if_par
-        real,intent(out) :: amin,amax
+    subroutine read_active
+        character(:),allocatable :: text
+        !e.g. text='vp1500:3400', will output:
+        !pars='vp', pars_min=1500, pars_max=3400
+        !  k1=2,    k2=7,   k3=11
 
-        i=index(active,par)
-        !print*,'i=',i
+        npar=0
 
-        if(i>0) then
-            if_par=.true.
-            j=index(active(i:),':')
-            !print*,'j=',j
+        loop: do while (npar<=3) !maximum 3 parameters
             
-            read(active(i+len(par):i+j-2),*) amin
-            
-            k=index(active(i:),' ')
-            if(k==0) k=len(active)+1-i
-            !print*,'k=',k
-            
-            read(active(i+j:i+k-1),*) amax
-            
-            !print*,par_min, par_max
-        else
-            if_par=.false.
+            if(len(active)==1) exit loop !no more parameters to read
+
+            k3=index(active,' ') !parameters are separated by spaces
+            if(k3==0) k3=len(active) !k3 can be 0 when looking at last parameter
+
+            text=active(1:k3)
+            if(len(text)==1) exit !no more parameters to read
+
+            !ignore parameters depending on parameterization
+            if(.not. check_par(text) ) then
+                !truncate active
+                active=trim(adjustl(active(k3:len(active))))
+
+                cycle loop
+            endif
+
+            !counter of valid parameters
+            npar=npar+1
+
+            !read in parameter
+            pars(npar)=text(1:2); k1=2
+            if (text(1:3)=='rho'.or.text(1:3)=='kpa'.or.text(1:3)=='lda') then
+                pars(npar)=text(1:3); k1=3
+            endif
+        
+            !read in parameter range
+            k2=index(text,':')
+            read(text(k1+1:k2-1),*) pars_min(npar)
+            read(text(k2+1:k3  ),*) pars_max(npar)
+
+            ! if(pars(i)=='vp') then
+            !         par_vp_min=pars_min(i)
+            !         par_vp_max=pars_max(i)
+            ! endif
+
+            !truncate active
+            active=trim(adjustl(active(k3:len(active))))
+
+            if(len(active)==1) exit loop !fin du active
+
+        enddo loop
+
+
+        if(mpiworld%is_master) then
+            write(*,*) 'Number of inversion parameters:', npar
+            write(*,*) 'Read parameters:',pars(1),' ',pars(2),' ',pars(3)
         endif
+
+
     end subroutine
+
+    function check_par(text)
+        character(*) :: text
+        logical :: check_par
+        
+        check_par=.true.
+        
+        if    (text(1:2)=='vp' ) then !P-wave velocity
+            check_par = index(parameterization,'velocities')>0
+            
+        elseif(text(1:2)=='vs' ) then !S-wave velocity
+            check_par = index(parameterization,'velocities')>0 .and. index(waveeq_info,'elastic')>0
+            
+        elseif(text(1:2)=='sp' ) then !P-wave slowness
+            check_par = index(parameterization,'slowness')>0
+            
+        elseif(text(1:2)=='ip' ) then !P-wave (acoustic) impedance
+            check_par = index(parameterization,'impedance')>0
+            
+        elseif(text(1:3)=='rho') then !density
+            check_par = index(parameterization,'density')>0
+            
+        elseif(text(1:3)=='kpa') then !bulk modulus
+            check_par = index(parameterization,'moduli')>0 .and. index(waveeq_info,'acoustic')>0
+            
+        elseif(text(1:3)=='lda') then !1st Lamé parameter
+            check_par = index(parameterization,'moduli')>0 .and. index(waveeq_info,'elastic')>0
+            
+        elseif(text(1:2)=='mu' ) then !2nd Lamé parameter, shear modulus
+            check_par = index(parameterization,'moduli')>0 .and. index(waveeq_info,'elastic')>0
+            
+        endif
+
+        if(.not. check_par) then !ce paramètre n'est plus actif
+            if(mpiworld%is_master) write(*,*) 'Parameter ',text(1:3),' will not be active in the inversion..'
+        endif
+        
+    end function
     
     subroutine parameterization_transform(dir,x,g)
         character(3) :: dir
@@ -96,41 +172,51 @@ use m_gradient, only: gradient
         
         !model
         if(dir=='m2x') then
-            select case (parameterization)
-                case ('vp-rho')
-                    if(if_par_vp)  x(:,:,:,1) = (m%vp -par_vp_min)  / (par_vp_max -par_vp_min)
-                    if(if_par_rho) x(:,:,:,2) = (m%rho-par_rho_min) / (par_rho_max-par_rho_min)
-                case ('vp-ip')
-                    if(if_par_vp)  x(:,:,:,1) = (m%vp -par_vp_min)  / (par_vp_max -par_vp_min)
-                    if(if_par_ip)  x(:,:,:,2) = (m%vp*m%rho -par_ip_min)  / (par_ip_max -par_ip_min)
-                end select
+            ! select case (parameterization)
+            !     case ('vp-rho')
+            !         if(if_par_vp)  x(:,:,:,1) = (m%vp -par_vp_min)  / (par_vp_max -par_vp_min)
+            !         if(if_par_rho) x(:,:,:,2) = (m%rho-par_rho_min) / (par_rho_max-par_rho_min)
+            !     case ('vp-ip')
+            !         if(if_par_vp)  x(:,:,:,1) = (m%vp -par_vp_min)  / (par_vp_max -par_vp_min)
+            !         if(if_par_ip)  x(:,:,:,2) = (m%vp*m%rho -par_ip_min)  / (par_ip_max -par_ip_min)
+            !     end select
+            do ipar=1,npar
+                if(pars(ipar)=='vp' ) x(:,:,:,ipar)= (m%vp      -pars_min(ipar))  / (pars_max(ipar) -pars_min(ipar))
+                if(pars(ipar)=='rho') x(:,:,:,ipar)= (m%rho     -pars_min(ipar))  / (pars_max(ipar) -pars_min(ipar))
+                if(pars(ipar)=='ip' ) x(:,:,:,ipar)= (m%vp*m%rho-pars_min(ipar))  / (pars_max(ipar) -pars_min(ipar))
+            enddo
         else 
-            select case (parameterization)
-!                 case ('kappa-rho')
-                case ('vp-rho')
-                    if(if_par_vp)  m%vp = x(:,:,:,1)*(par_vp_max -par_vp_min )+par_vp_min
-                    if(if_par_rho) m%rho= x(:,:,:,2)*(par_rho_max-par_rho_min)+par_rho_min
-!                case ('vprho-gardner')
-                case ('vp-ip')
-                    if(if_par_vp)  m%vp = x(:,:,:,1)*(par_vp_max -par_vp_min )+par_vp_min
-                    if(if_par_ip)  m%rho=(x(:,:,:,2)*(par_ip_max -par_ip_min )+par_ip_min) /m%vp
-            end select
+!             select case (parameterization)
+! !                 case ('kappa-rho')
+!                 case ('vp-rho')
+!                     if(if_par_vp)  m%vp = x(:,:,:,1)*(par_vp_max -par_vp_min )+par_vp_min
+!                     if(if_par_rho) m%rho= x(:,:,:,2)*(par_rho_max-par_rho_min)+par_rho_min
+! !                case ('vprho-gardner')
+!                 case ('vp-ip')
+!                     if(if_par_vp)  m%vp = x(:,:,:,1)*(par_vp_max -par_vp_min )+par_vp_min
+!                     if(if_par_ip)  m%rho=(x(:,:,:,2)*(par_ip_max -par_ip_min )+par_ip_min) /m%vp
+!             end select
+            do ipar=1,npar
+                if(pars(ipar)=='vp' ) m%vp = x(:,:,:,ipar)*(pars_max(ipar)-pars_min(ipar)) + pars_min(ipar)
+                if(pars(ipar)=='rho') m%rho= x(:,:,:,ipar)*(pars_max(ipar)-pars_min(ipar)) + pars_min(ipar)
+                if(pars(ipar)=='ip' ) m%rho=(x(:,:,:,ipar)*(pars_max(ipar)-pars_min(ipar)) + pars_min(ipar)) /m%vp
+            enddo
         endif
         
         
         !gradient
         if(present(g)) then
         if(dir=='m2x') then
-            select case (parameterization)
-    !             case ('kappa-rho')
-                case ('vp-rho') !unit of g = [kg/m/s2]
-                    if(if_par_vp)  g(:,:,:,1)=(gradient(:,:,:,1)*2.*m%vp*m%rho)               *(par_vp_max-par_vp_min)
-                    if(if_par_rho) g(:,:,:,2)=(gradient(:,:,:,1)*m%vp*m%vp+gradient(:,:,:,2)) *(par_rho_max-par_rho_min)
-    !             case ('vprho-gardner')
-                case ('vp-ip')
-                    if(if_par_vp) g(:,:,:,1)=(gradient(:,:,:,1)*m%vp*m%rho - gradient(:,:,:,2)*m%rho/m%vp) *(par_vp_max-par_vp_min)
-                    if(if_par_ip) g(:,:,:,2)=(gradient(:,:,:,1)*m%vp       + gradient(:,:,:,2)/m%vp      ) *(par_ip_max-par_ip_min)
-            end select
+    !         select case (parameterization)
+    ! !             case ('kappa-rho')
+    !             case ('vp-rho') !unit of g = [kg/m/s2]
+    !                 if(if_par_vp)  g(:,:,:,1)=(gradient(:,:,:,1)*2.*m%vp*m%rho)               *(par_vp_max-par_vp_min)
+    !                 if(if_par_rho) g(:,:,:,2)=(gradient(:,:,:,1)*m%vp*m%vp+gradient(:,:,:,2)) *(par_rho_max-par_rho_min)
+    ! !             case ('vprho-gardner')
+    !             case ('vp-ip')
+    !                 if(if_par_vp) g(:,:,:,1)=(gradient(:,:,:,1)*m%vp*m%rho - gradient(:,:,:,2)*m%rho/m%vp) *(par_vp_max-par_vp_min)
+    !                 if(if_par_ip) g(:,:,:,2)=(gradient(:,:,:,1)*m%vp       + gradient(:,:,:,2)/m%vp      ) *(par_ip_max-par_ip_min)
+    !         end select
         endif
         endif
         
