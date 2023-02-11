01609fa285e753b7e682de86d7da0d246b2e2eff
joeyartech <{ID}+{username}@users.noreply.github.com>
2023-02-11 18:09:01 +0300

    correct Demo/01_ForwardModeling

--- Demo/01_ForwardModeling/go.sh
+++ Demo/01_ForwardModeling/go.sh
@@ -7,8 +7,8 @@
 
 makevel nz=201 nx=201 v000=1000 > model
 
-../../exe/fwd_AC_FDSG_O4  setup.in
+../../exe/fwd_AC_FDSG_O4  setup.in > out
 
-suwind < dsyn_0001.su key=trid min=11 max=11 | suximage perc=99 legend=1 title=p &
-suwind < dsyn_0001.su key=trid min=12 max=12 | suximage perc=99 legend=1 title=vz &
-suwind < dsyn_0001.su key=trid min=13 max=13 | suximage perc=99 legend=1 title=vx &
+suwind < results/dsyn_Shot0001.su key=trid min=11 max=11 | suximage perc=99 legend=1 title=p &
+suwind < results/dsyn_Shot0001.su key=trid min=12 max=12 | suximage perc=99 legend=1 title=vz &
+suwind < results/dsyn_Shot0001.su key=trid min=14 max=14 | suximage perc=99 legend=1 title=vx &
